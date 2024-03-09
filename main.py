4684514577685316167204b566180dea70923948
saltchicken <john.eicher89@gmail.com>
2024-03-08 19:59:09 -0800

    Use np.copy in read

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class Transceiver():
     def read(self):
         for i in range(self.num_samps//self.buffer_size):
             self.rx_streamer.recv(self.recv_buffer, self.rx_metadata)
-            self.samples[i*1000:(i+1)*1000] = self.recv_buffer[0]
+            self.samples[i*1000:(i+1)*1000] = np.copy(self.recv_buffer[0])
         if not self.rx_metadata.error_code == uhd.types.RXMetadataErrorCode.none:
             logger.warning(self.rx_metadata.error_code)
         return self.samples
