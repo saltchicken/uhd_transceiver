908dab58bf64b9ac9480ff42880d85ca62e3ab9f
saltchicken <john.eicher89@gmail.com>
2024-03-10 19:20:53 -0700

    Revert "Fix typo"
    This reverts commit e70d945450d9ff78710e307e962e33ab6e9182bc.

--- server.py
+++ server.py
@@ -80,9 +80,10 @@ class Transceiver():
                 self.rx_node.join()
         except KeyboardInterrupt as e:
             logger.info("Keyboard interrupt hit")
-            self.rx_node.stop()
-            self.rx_node.join()
-
+            if self.rx_node:
+                self.rx_node.stop()
+                self.rx_node.join()
+            else:
                 
             
     
